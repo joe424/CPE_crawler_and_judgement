1e1d3b85649daa38ff17b8e6df7a698cd87d4f0a
joe <joe424joe1113@gmail.com>
2021-03-07 16:37:04 +0800

    202103070437

---
+++ README.md
@@ -0,0 +1,22 @@
+# CPE_crawler_and_judgement
+:::warning
+目前只能用在windows
+:::
+
+安裝第三方模組:
+```
+pip install -r requirements.txt
+```
+<br/>
+
+使用 `crawler.py` 來爬取CPE題目:
+> 若有新考題且要爬取也可使用此指令
+```
+python crawler.py
+```
+<br/>
+
+若要 `compare.exe` 有更新且要加入到所有考題的資料夾內:
+```
+python upgrade.py
+```
--- compare.cpp
+++ compare.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include <regex>
 #include <fstream>
-
-#ifdef _WIN32
-#include <winows.h>
-#endif
+#include <windows.h>
 
 using namespace std;
 
@@ -25,13 +22,7 @@ int main(int argc, char *argv[]){
         if(count == 0)
             cout << "[Error]: no *.c or *.cpp file" << endl;
 
-        #ifdef __linux__
-            string str;
-            cout << "press any key to continue";
-            getline(cin, str);
-        #elif _WIN32
-            system("PAUSE");
-        #endif
+        system("PAUSE");
 
         return 0;
     }
@@ -44,11 +35,8 @@ int main(int argc, char *argv[]){
     ifstream test1_out("test1.out");
     if(test1_in.good() && test1_out.good()){
 
-        #ifdef __linux__
-            system("./a < test1.in > test1.in.out");
-        #elif _WIN32
-            system("a.exe < test1.in >> test1.in.out");
-        #endif
+
+        system("a.exe < test1.in >> test1.in.out");
 
         ifstream test1_in_out("test1.in.out");
         
@@ -60,62 +48,46 @@ int main(int argc, char *argv[]){
                 all_right = false;
             // cout << "[Wrong answer in test1]" << endl;
             // cout << "      input: " << line0 << endl;
-            #ifdef __linux__
-                cout << "your answer: " << line1 << endl;
-                cout << "real answer: " << line2 << endl;
-                cout << endl;
-            #elif _WIN32
-                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-                if(line1 == line2){
-                    cout << "your answer: ";
-                    SetConsoleTextAttribute(hConsole, 10);
-                    cout << line1 << endl;
-                    SetConsoleTextAttribute(hConsole, 7);
-                    cout << "real answer: ";
-                    SetConsoleTextAttribute(hConsole, 10);
-                    cout << line2 << endl;
-                    cout << endl;
-                }else{
-                    cout << "your answer: ";
-                    SetConsoleTextAttribute(hConsole, 12);
-                    cout << line1 << endl;
-                    SetConsoleTextAttribute(hConsole, 7);
-                    cout << "real answer: ";
-                    SetConsoleTextAttribute(hConsole, 12);
-                    cout << line2 << endl;
-                    cout << endl;
-                }
+            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+            if(line1 == line2){
+                cout << "your answer: ";
+                SetConsoleTextAttribute(hConsole, 10);
+                cout << line1 << endl;
                 SetConsoleTextAttribute(hConsole, 7);
-            #endif
+                cout << "real answer: ";
+                SetConsoleTextAttribute(hConsole, 10);
+                cout << line2 << endl;
+                cout << endl;
+            }else{
+                cout << "your answer: ";
+                SetConsoleTextAttribute(hConsole, 12);
+                cout << line1 << endl;
+                SetConsoleTextAttribute(hConsole, 7);
+                cout << "real answer: ";
+                SetConsoleTextAttribute(hConsole, 12);
+                cout << line2 << endl;
+                cout << endl;
+            }
+            SetConsoleTextAttribute(hConsole, 7);
             
         }
-        #ifdef __linux__
-            if(all_right == true)
-                cout << "========= PASS test1 =========" << endl << endl;
-            else
-                cout << "========= test1 NOT passed =========" << endl << endl;
-        #elif _WIN32
-            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-            if(all_right == true){
-                SetConsoleTextAttribute(hConsole, 47);
-                cout << "========= PASS test1 =========";
-                SetConsoleTextAttribute(hConsole, 7);
-                cout << endl << endl;
-            }else{
-                SetConsoleTextAttribute(hConsole, 79);
-                cout << "========= test1 NOT passed =========";
-                SetConsoleTextAttribute(hConsole, 7);
-                cout << endl << endl;
-            }
-        #endif
+
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        if(all_right == true){
+            SetConsoleTextAttribute(hConsole, 47);
+            cout << "========= PASS test1 =========";
+            SetConsoleTextAttribute(hConsole, 7);
+            cout << endl << endl;
+        }else{
+            SetConsoleTextAttribute(hConsole, 79);
+            cout << "========= test1 NOT passed =========";
+            SetConsoleTextAttribute(hConsole, 7);
+            cout << endl << endl;
+        }
 
         test1_in_out.close();
 
-        #ifdef __linux__
-            system("rm test1.in.out");
-        #elif _WIN32
-            system("del test1.in.out");
-        #endif
+        system("del test1.in.out");
 
         analysis = true;
     }
@@ -127,11 +99,7 @@ int main(int argc, char *argv[]){
     ifstream test2_out("test2.out");
     if(test2_in.good() && test2_out.good()){
 
-        #ifdef __linux__
-            system("./a < test2.in > test2.in.out");
-        #elif _WIN32
-            system("a.exe < test2.in >> test2.in.out");
-        #endif
+        system("a.exe < test2.in >> test2.in.out");
 
         ifstream test2_in_out("test2.in.out");
         
@@ -143,61 +111,44 @@ int main(int argc, char *argv[]){
                 all_right = false;
             // cout << "[Wrong answer in test2]" << endl;
             // cout << "      input: " << line0 << endl;
-            #ifdef __linux__
-                cout << "your answer: " << line1 << endl;
-                cout << "real answer: " << line2 << endl;
-                cout << endl;
-            #elif _WIN32
-                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-                if(line1 == line2){
-                    cout << "your answer: ";
-                    SetConsoleTextAttribute(hConsole, 10);
-                    cout << line1 << endl;
-                    SetConsoleTextAttribute(hConsole, 7);
-                    cout << "real answer: ";
-                    SetConsoleTextAttribute(hConsole, 10);
-                    cout << line2 << endl;
-                    cout << endl;
-                }else{
-                    cout << "your answer: ";
-                    SetConsoleTextAttribute(hConsole, 12);
-                    cout << line1 << endl;
-                    SetConsoleTextAttribute(hConsole, 7);
-                    cout << "real answer: ";
-                    SetConsoleTextAttribute(hConsole, 12);
-                    cout << line2 << endl;
-                    cout << endl;
-                }
-                SetConsoleTextAttribute(hConsole, 7);
-            #endif
-        }
-        #ifdef __linux__
-            if(all_right == true)
-                cout << "========= PASS test2 =========" << endl << endl;
-            else
-                cout << "========= test2 NOT passed =========" << endl << endl;
-        #elif _WIN32
             HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-            if(all_right == true){
-                SetConsoleTextAttribute(hConsole, 47);
-                cout << "========= PASS test2 =========";
+            if(line1 == line2){
+                cout << "your answer: ";
+                SetConsoleTextAttribute(hConsole, 10);
+                cout << line1 << endl;
                 SetConsoleTextAttribute(hConsole, 7);
-                cout << endl << endl;
+                cout << "real answer: ";
+                SetConsoleTextAttribute(hConsole, 10);
+                cout << line2 << endl;
+                cout << endl;
             }else{
-                SetConsoleTextAttribute(hConsole, 79);
-                cout << "========= test2 NOT passed =========";
+                cout << "your answer: ";
+                SetConsoleTextAttribute(hConsole, 12);
+                cout << line1 << endl;
                 SetConsoleTextAttribute(hConsole, 7);
-                cout << endl << endl;
+                cout << "real answer: ";
+                SetConsoleTextAttribute(hConsole, 12);
+                cout << line2 << endl;
+                cout << endl;
             }
-        #endif
+            SetConsoleTextAttribute(hConsole, 7);
+        }
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        if(all_right == true){
+            SetConsoleTextAttribute(hConsole, 47);
+            cout << "========= PASS test2 =========";
+            SetConsoleTextAttribute(hConsole, 7);
+            cout << endl << endl;
+        }else{
+            SetConsoleTextAttribute(hConsole, 79);
+            cout << "========= test2 NOT passed =========";
+            SetConsoleTextAttribute(hConsole, 7);
+            cout << endl << endl;
+        }
 
         test2_in_out.close();
 
-        #ifdef __linux__
-            system("rm test2.in.out");
-        #elif _WIN32
-            system("del test2.in.out");
-        #endif
+        system("del test2.in.out");
         
         analysis = true;
     }
@@ -210,11 +161,7 @@ int main(int argc, char *argv[]){
         cout << "remove execution file?(y/n): ";
         getline(cin, remove);
         if(remove == "y" || remove == "Y" || remove == ""){
-            #ifdef __linux__
-                system("rm a");
-            #elif _WIN32
-                system("del a.exe");
-            #endif
+            system("del a.exe");
         }
     }
 }
